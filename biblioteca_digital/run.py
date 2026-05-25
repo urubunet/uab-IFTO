2dc1e97a574bca67a148a42406bbbf3343c28e27
urubunet <urubunet@gmail.com>
2026-05-25 08:51:36 -0300

    Ajuste de log de inicialização do servidor

--- biblioteca_digital/run.py
+++ biblioteca_digital/run.py
@@ -5,4 +5,9 @@ app = criar_app()
 app.secret_key = Config.SECRET_KEY
 
 if __name__ == '__main__':
+    # Configurar logger para garantir logs de inicialização visíveis
+    import logging
+    log = logging.getLogger('werkzeug')
+    log.setLevel(logging.INFO)
+    
     app.run(debug=Config.DEBUG_MODE, host='0.0.0.0', port=5000)
